8837d08756d41096e782db73acf5e8c679e35457
DAMO-13 <neo4130@gmail.com>
2021-08-01 22:09:32 +0900

    playground 수정

--- playGround.cpp
+++ playGround.cpp
@@ -15,27 +15,38 @@ playGround::~playGround()
 HRESULT playGround::init()
 {
 	gameNode::init(true);
-	
+
 	IMAGEMANAGER->addImage("background", "Level 1 - Frozen Suburbs.bmp", 31812, 1400, true, MAGENTA);
-	IMAGEMANAGER->addImage("배경", "background.bmp", WINSIZEX, WINSIZEY, true,
+	IMAGEMANAGER->addImage("배경", "backdu bisch ein archground.bmp", WINSIZEX, WINSIZEY, true,
 		RGB(255, 0, 255));
 	
-	
+	INIDATA->addData("선우", "코딩력", "10");
+	INIDATA->addData("선우", "방구석여포력", "100");
+	INIDATA->addData("선우", "형들에게이쁨받는력", "94");
 
-	_posX = 200;
+	INIDATA->addData("문철", "코딩력", "800");
+	INIDATA->addData("문철", "신성력", "1000");
+	INIDATA->addData("문철", "부품판별력", "500");
+
+	INIDATA->addData("문철", "코딩력", "500");
+	INIDATA->addData("문철", "신성력", "1000");
+	INIDATA->addData("문철", "부품판별력", "500");
+
+	INIDATA->iniSave("29기");
+
+	_str = INIDATA->loadDataString("29기", "문철", "신성력");
+	_power = INIDATA->loadDataInterger("29기", "문철", "신성력");
+
+	_posX = 27000;
 	_posY = WINSIZEY / 2;
+
+	_player = new Ramona;
+	_player->init();
+	_player->InitVariables();
+
 
 	_cm = new CameraManager;
 	_cm->init(31812, 1400);
-
-	_em = new EnemyManager;
-	_em->init();
-	_em->SetCamera(_cm);
-	_em->SetLuke();
-	_em->SetMike();
-	_em->SetMalcolm();
-	
-	
 
 	return S_OK;
 }
@@ -53,20 +64,10 @@ void playGround::update()
 {
 	gameNode::update();
 
-	
-	if (KEYMANAGER->isStayKeyDown(VK_LEFT)) _posX -= 5;
-	if (KEYMANAGER->isStayKeyDown(VK_RIGHT)) _posX += 5;
-	if (KEYMANAGER->isStayKeyDown(VK_UP)) _posY -= 5;
-	if (KEYMANAGER->isStayKeyDown(VK_DOWN)) _posY += 5;
+	_player->update();
+	_cm->update(_player->getPosX(), _player->getPosY());
 
-	
-
-	_cm->update(_posX, _posY);
-	
-	_em->update();
-
-	selectionSort();
-
+	RENDERMANAGER->update();
 }
 
 //여기다 그려줘라!!!
@@ -77,76 +78,26 @@ void playGround::render()
 	
 	char str[256];
 	HFONT oldFont, font1;
-	/*
-	CreateFont(
-		40,			//문자폭
-		0,			//문자 넓이
-		40,			//문자 기울기(진짜 기울어짐)
-		0,			//문자 방향
-		600,		//문자 굵기
-		0,			//bool 문자 기울기 이탤릭체 사용유무
-		0,			//bool 문자 밑줄 
-		0,			//bool 문자 취소선 (나무위키에 많은 그것)
-		HANGUL_CHARSET,	//문자 셋팅
-		0,				//출력 정확도
-		0,				//클리핑 정확도
-		0,				//출력의 퀄리티
-		0,				//자간
-		TEXT("궁서체"));	//폰트 
-	*/
-	/*
-	
-
-	font1 = CreateFont(50, 0, 0, 0, 100, true, true, true, HANGUL_CHARSET, 0, 0, 0, 50, TEXT("휴먼굴림"));
-	oldFont = (HFONT)SelectObject(getMemDC(), font1);
-	sprintf_s(str, "울트라 캡숑 나이쓰");
-	TextOut(getMemDC(), 10, 100, str, strlen(str));
-	DeleteObject(font1);
-	*/
-
-	SetTextColor(getMemDC(), RGB(0, 0, 0));
 
 	IMAGEMANAGER->findImage("background")->render(getMemDC(), 0, 0, _cm->getCamX(), _cm->getCamY(), WINSIZEX, WINSIZEY);
+	_player->render(_cm->getRenderPosX(), _cm->getRenderPosY());
 
-	_em->render();
-	RectangleMakeCenter(getMemDC(), _cm->getRenderPosX(), _cm->getRenderPosY(), 50, 81);
-
-	sprintf_s(str, "char X : %d", _posX);
+	sprintf_s(str, "char X : %d", _player->getPosX());
 	TextOut(getMemDC(), 100, 90, str, strlen(str));
 	
 	sprintf_s(str, "render X : %d", _cm->getRenderPosX());
 	TextOut(getMemDC(), 100, 110, str, strlen(str));
 
 
-	sprintf_s(str, "char Y : %d", _posY);
+	sprintf_s(str, "char Y : %d", _player->getPosY());
 	TextOut(getMemDC(), 100, 130, str, strlen(str));
 
 	sprintf_s(str, "render Y : %d", _cm->getRenderPosY());
 	TextOut(getMemDC(), 100, 150, str, strlen(str));
 
 
-	RECT rcText = RectMake(10, 300, 400, 100);
 
-	HFONT font2 = CreateFont(40, 0, 0, 0, 300, false, false, false,
-		DEFAULT_CHARSET, OUT_STROKE_PRECIS, CLIP_DEFAULT_PRECIS,
-		PROOF_QUALITY, DEFAULT_PITCH | FF_SWISS, TEXT("PokemonGSC"));
-
-	HFONT oldFont2 = (HFONT)SelectObject(getMemDC(), font2);
-
-	//DrawText(getMemDC(), TEXT("TEXT"), 7, &rcText, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-
-	for (int i = 0; i < 10; i++)
-	{
-		//sprintf_s(str, "%d", _arr[i]);
-		//TextOut(getMemDC(), 50 + 50 * i, 90, str, strlen(str));
-	}
-
-	//DrawText(getMemDC(), TEXT("Subway"), 7, &rcText, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-	SelectObject(getMemDC(), oldFont2);
-	DeleteObject(font2);
-
-	
-
+	RENDERMANAGER->render(getMemDC());
 	TIMEMANAGER->render(getMemDC());
 
 	//=============== 밑에도 건들지마라 ================
@@ -154,28 +105,5 @@ void playGround::render()
 
 }
 
-void playGround::selectionSort()
-{
-		int minIndex;
-		int i, j;
-		for (i = 0; i < 10 - 1; i++) {
-			minIndex = i;
-			for (j = i + 1; j < 10; j++)
-				if (_arr[j] < _arr[minIndex])
-					minIndex = j;
-
-			swap(i, minIndex);
-		}
-}
-
-void playGround::swap(int index, int minIndex)
-{
-	int temp;
-
-	temp = _arr[index];
-	_arr[index] = _arr[minIndex];
-	_arr[minIndex] = temp;
-
-}
 
 
--- playGround.h
+++ playGround.h
@@ -2,19 +2,14 @@
 #include "gameNode.h"
 #include "string"
 #include "CameraManager.h"
-#include "EnemyManager.h"
-#include "Enemy.h"
-#include "restaurantScene.h"
-
+#include "Ramona.h"
+#define ARRSIZE 10
 
 class playGround : public gameNode
 {
 private:
 	CameraManager* _cm;
-	RECT _player;
-	EnemyManager* _em;
-	Enemy* _enemy;
-	
+	player* _player;
 
 	int _posX;
 	int _posY;
@@ -23,7 +18,7 @@ private:
 
 	int _power;
 
-	int _arr[10] = { 2 , 5 ,1 , 4 , 7 , 8 , 9, 3, 11, 10 };
+	int _arr[ARRSIZE] = { 2 , 5 ,1 , 4 , 7 , 8 , 9, 3, 11, 10 };
 
 public:
 	playGround();
@@ -33,8 +28,5 @@ public:
 	virtual void release();			//메모리 해제 함슈
 	virtual void update();			//연산하는 함수
 	virtual void render();			//그리기 함수
-
-	void selectionSort();
-	void swap(int index, int minIndex);
 };
 
